f4aeeb0814208f5e74780b6a5178e09363752858
Shen Shen <shenshen@mit.edu>
2023-07-04 00:47:07 -0400

    jensen legend

--- demos/scripts/Jensen.py
+++ demos/scripts/Jensen.py
@@ -34,6 +34,7 @@ def vertical_line(x, y):
         mode="lines",
         name="Vertical Line",
         marker=dict(color=c.BLUE),
+        showlegend=False,
     )
 
 
@@ -45,6 +46,7 @@ def horizontal_line(x, y):
         mode="lines",
         name="Horizontal Line",
         marker=dict(color=c.PURPLE),
+        showlegend=False,
     )
 
 
@@ -56,6 +58,7 @@ def scatter_on_f(i=alphas[0]):
             mode="markers",
             marker=dict(size=4, showscale=False),
             hovertemplate="x: %{x}<br>y: %{y}",
+            showlegend=False,
         ),
         horizontal_line(xbar, f_of_x_bar[str(i)]),
         vertical_line(xbar, f_of_x_bar[str(i)]),
@@ -70,15 +73,17 @@ subplot_x_axis = [
         xaxis="x",
         yaxis="y2",
         marker=dict(size=4, showscale=False),
+        showlegend=False,
     ),
     go.Scatter(
         x=np.array(xbar),
         y=np.zeros(1),
-        mode="markers",
         xaxis="x",
         yaxis="y2",
+        mode="markers",
         marker=dict(size=12, symbol="star", color=c.BLUE),
         hovertemplate="the mean of x samples: %{xbar}",
+        name="$\mathbb{E}[x]$",
     ),
 ]
 
@@ -92,22 +97,27 @@ def subplot_y_axis(i=alphas[0]):
             yaxis="y",
             mode="markers",
             marker=dict(size=4, showscale=False),
+            showlegend=False,
         ),
         go.Scatter(
             x=np.zeros(1),
             y=[ybar[str(i)]],
             xaxis="x2",
             yaxis="y",
+            mode="markers",
             marker=dict(size=12, symbol="square", color=c.RED),
             hovertemplate="the mean of f(x): %{ybar}",
+            name="$\mathbb{E}[f(x)]$",
         ),
         go.Scatter(
             x=np.zeros(1),
             y=[f_of_x_bar[str(i)]],
             xaxis="x2",
             yaxis="y",
+            mode="markers",
             marker=dict(size=12, symbol="star", color=c.PURPLE),
             hovertemplate="f of the mean of x: %{f_of_x_bar[0]}",
+            name="$f(\mathbb{E}[x])$",
         ),
     ]
 
@@ -149,7 +159,7 @@ fig = go.Figure(
         sliders=[
             dict(
                 active=0,
-                currentvalue={"prefix": r"$\alpha$: "},
+                currentvalue={"prefix": "α: "},
                 pad={"t": 50},
                 steps=[
                     dict(
@@ -177,21 +187,26 @@ fig = go.Figure(
     ],
 )
 
+legend = dict(orientation="v", x=0.16, y=0.16)
 # Show the figure
 fig.update_layout(
     title="Jensen's Inequality",
-    xaxis=dict(title="Sampled x", domain=[0.15, 1]),
-    yaxis=dict(title=r"$f(x)$", domain=[0.15, 1], tickformat="$%s$"),
+    xaxis=dict(
+        title="Sampled $x$",
+        domain=[0.15, 1],
+    ),
+    yaxis=dict(title="$f(x)$", domain=[0.15, 1], tickformat="$%s$"),
     xaxis2=dict(title=None, domain=[0, 0.03], range=(-0.1, 0.1), showticklabels=False),
     yaxis2=dict(title=None, domain=[0, 0.03], range=(-0.1, 0.1), showticklabels=False),
-    showlegend=False,
+    showlegend=True,
+    legend=legend,
 )
 
-
-# Create the figure
-# fig.show()
 
 import os
 
 script_folder = os.path.dirname(os.path.abspath(__file__))
-fig.write_html(script_folder + "/../Jensen.html")
+fig.write_html(
+    script_folder + "/../Jensen.html",
+    include_mathjax="/assets/js/MathJax/MathJax.js",
+)

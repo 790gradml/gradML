76971edd4d0d2c609018f2bb7cea0a55d3a96f70
Shen Shen <shenshen@mit.edu>
2023-07-08 00:14:44 -0400

    quadratic demo touch up

--- demos/scripts/QuadraticFun.py
+++ demos/scripts/QuadraticFun.py
@@ -1,11 +1,8 @@
 import plotly.graph_objects as go
 import numpy as np
+import colors as c
 
-
-# TODO: use proper LaTeX instead of Unicode hack
-
-# from pandas import DataFrame
-# Create figure
+# todo: add non-unique min
 fig = go.Figure()
 r_max = [1, 1.5]
 x = np.linspace(-r_max[0], r_max[0], 60)
@@ -27,10 +24,10 @@ Qs = [
 
 qs = [np.array([1, 2]), np.array([0, 0]), np.array([-10, 0]), np.array([0, 0])]
 titles = [
-    "f(x₁,x₂)=4x₁²+5x₂²+4x₁x₂+x₁+2x₂, f strictly convex",
-    "f(x₁,x₂)=x₁²+4x₂²+4x₁x₂, f convex but not strictly convex",
-    "f(x₁,x₂)=x₁²+x₂²+2x₁x₂-10x₁, f convex but not strictly convex",
-    "f(x₁,x₂)=-x₁²+4x₂²+4x₁x₂, f non-convex",
+    "$f(x_1, x_2)=4 x_1^2+5 x_2^2+4 x_1 x_2+x_1+2 x_2, f \mathrm{\ is \ strictly \ convex}$",
+    "$f(x_1, x_2)=x_1^2+4 x_2^2+4 x_1 x_2, f \mathrm {\ is \ convex \ but \ not \ strictly \ convex}$",
+    "$f(x₁,x₂)=x₁²+x₂²+2x₁x₂-10x₁, f \mathrm{\ is \ convex \ but \ not \ strictly \ convex}$",
+    "$f(x₁,x₂)=-x₁²+4x₂²+4x₁x₂, f \mathrm {\ is \ non-convex}$",
 ]
 
 labels = [
@@ -42,7 +39,15 @@ labels = [
 
 a = np.array([-0.0312])
 b = np.array([-0.1875])
-fig.add_trace(go.Scatter3d(x=a, y=b, z=quadratic(a, b, Qs[0], qs[0])))
+fig.add_trace(
+    go.Scatter3d(
+        x=a,
+        y=b,
+        z=quadratic(a, b, Qs[0], qs[0]),
+        name="Unique Minimum",
+        marker=dict(size=12, symbol="diamond", color=c.BLUE),
+    )
+)
 # Add traces, one for each slider step
 pi = np.pi
 cos = np.cos
@@ -59,7 +64,10 @@ for Q, q in zip(Qs, qs):
             y=Y,
             z=Z,
             colorscale="Sunset",
+            showscale=False,
             opacity=1,
+            showlegend=True,
+            name="\Quadratic function $f(x_1,x_2)$",
         )
     )
 
@@ -87,11 +95,13 @@ for j, i in enumerate(Qs):
 sliders = [
     dict(
         active=10,
-        currentvalue={"prefix": "Q: "},
+        currentvalue={"prefix": "Hessian matrix Q: "},
         pad={"t": 50},
         steps=steps,
     )
 ]
+
+# https://github.com/plotly/plotly.js/issues/608
 
 fig.update_layout(
     sliders=sliders,
@@ -100,16 +110,14 @@ fig.update_layout(
     title_font_size=20,
     showlegend=False,
     scene=dict(
-        xaxis=dict(title="x₁"),
-        yaxis=dict(title=r"x₂"),
-        zaxis=dict(title=r"f(x₁,x₂)", nticks=4),
+        xaxis=dict(title="x₁"), yaxis=dict(title="x₂"), zaxis=dict(title="f(x₁,x₂)")
     ),
 )
 
-
-fig.update_layout()
-fig.show()
 import os
 
 script_folder = os.path.dirname(os.path.abspath(__file__))
-fig.write_html(script_folder + "/../QuadraticFun.html")
+fig.write_html(
+    script_folder + "/../QuadraticFun.html",
+    include_mathjax="/assets/js/MathJax/MathJax.js",
+)
